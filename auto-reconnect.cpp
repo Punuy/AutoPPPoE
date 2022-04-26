660aef77b0ebcdc1c634086e5b30444b6d1d5821
Punuy <67164516+Punuy@users.noreply.github.com>
2022-04-27 01:19:07 +0700

    Update auto-reconnect.cpp

--- auto-reconnect.cpp
+++ auto-reconnect.cpp
@@ -3,11 +3,10 @@
 #include <thread>
 
 using namespace std::chrono_literals;
-using namespace std;
 
 int main() {
     for (; ; ) {
-        system("rasdial PPPoE Username Password");
+        system("rasdial PPPoE 9604376325 password");
         std::this_thread::sleep_for(45s);
         system("rasdial PPPoE /disconnect");
     }
